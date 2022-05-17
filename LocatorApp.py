74bae0ab02a92edc1f1f7253ee9a92c6aee9c27b
chamrouensann <96223936+chamrouensqa@users.noreply.github.com>
2022-05-17 15:40:35 +0700

    Click action using find element by index

---
+++ LocatorApp.py
@@ -0,0 +1,19 @@
+from appium import webdriver
+from appium.webdriver.common.appiumby import AppiumBy
+
+desired_caps = {}
+desired_caps['platformName'] = 'Android'
+desired_caps['platformVersion'] = '10'
+desired_caps['deviceName'] = 'Pixel-3'
+desired_caps['automationName'] = 'UiAutomator2'
+desired_caps['appPackage'] = 'com.code2lead.kwad'
+desired_caps['appActivity'] = 'com.code2lead.kwad.MainActivity'
+desired_caps['app'] = ('/Users/chamrouen/Downloads/Android_Demo_App.apk')
+
+driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub',desired_caps)
+
+ele_index = driver.find_element(AppiumBy.ANDROID_UIAUTOMATOR,"UiSelector().index(4)")
+ele_index.click()
+
+
+
