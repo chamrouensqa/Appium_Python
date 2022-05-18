ed945902a05d6ce94c102f94f21c6a51921a156a
chamrouensann <96223936+chamrouensqa@users.noreply.github.com>
2022-05-18 08:39:02 +0700

    Adding action by Text

---
+++ ByText.py
@@ -0,0 +1,20 @@
+from appium import webdriver
+from appium.webdriver.common.appiumby import AppiumBy
+import time
+
+desired_caps = {}
+desired_caps['platformName'] = 'Android'
+desired_caps['platformVersion'] = '10'
+desired_caps['deviceName'] = 'Pixel-3'
+desired_caps['automationName'] = 'UiAutomator2'
+desired_caps['appPackage'] = 'com.code2lead.kwad'
+desired_caps['appActivity'] = 'com.code2lead.kwad.MainActivity'
+desired_caps['app'] = ('/Users/chamrouen/Downloads/Android_Demo_App.apk')
+
+driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub',desired_caps)
+
+ele_xpath = driver.find_element(AppiumBy.XPATH, '//android.widget.Button[@text="ENTER SOME VALUE"]')
+ele_xpath.click()
+
+time.sleep(2)
+driver.quit()
