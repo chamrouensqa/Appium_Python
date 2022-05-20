ff2f093b59cf38894d6ecd1993abec7ca5b260ab
chamrouensann <96223936+chamrouensqa@users.noreply.github.com>
2022-05-20 08:50:11 +0700

    Checking element

---
+++ ElementsProperty.py
@@ -0,0 +1,23 @@
+import time
+
+from appium import webdriver
+from appium.webdriver.common.appiumby import AppiumBy
+
+desired_caps = {}
+desired_caps['platformName'] = 'Android'
+desired_caps['platformVersion'] = '10'
+desired_caps['deviceName'] = 'Pixel-3'
+desired_caps['automationName'] = 'UiAutomator2'
+desired_caps['appPackage'] = 'com.code2lead.kwad'
+desired_caps['appActivity'] = 'com.code2lead.kwad.MainActivity'
+desired_caps['app'] = ('/Users/chamrouen/Downloads/Android_Demo_App.apk')
+
+driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub',desired_caps)
+
+check_ele = driver.find_element(AppiumBy.ID,"com.code2lead.kwad:id/EnterValue")
+
+print("Is element is display",check_ele.is_displayed())
+print("Is element is enable",check_ele.is_enabled())
+print("Is element is selected",check_ele.is_selected())
+print("Is element is size",check_ele.size)
+print("Is element is location",check_ele.location)
