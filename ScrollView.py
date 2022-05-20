8b31f26ec3bf1186dcdb1d7c48530e6ce816cc68
chamrouensann <96223936+chamrouensqa@users.noreply.github.com>
2022-05-20 10:06:03 +0700

    Adding scroll view to the automation option

---
+++ ScrollView.py
@@ -0,0 +1,28 @@
+import time
+
+from appium import webdriver
+from appium.webdriver.common.appiumby import AppiumBy
+from selenium.common.exceptions import ElementNotVisibleException, ElementNotSelectableException, NoSuchElementException
+from selenium.webdriver.support.wait import WebDriverWait
+
+desired_caps = {}
+desired_caps['platformName'] = 'Android'
+desired_caps['platformVersion'] = '10'
+desired_caps['deviceName'] = 'Pixel-3'
+desired_caps['automationName'] = 'UiAutomator2'
+desired_caps['appPackage'] = 'com.code2lead.kwad'
+desired_caps['appActivity'] = 'com.code2lead.kwad.MainActivity'
+desired_caps['app'] = ('/Users/chamrouen/Downloads/Android_Demo_App.apk')
+
+driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub',desired_caps)
+
+wait = WebDriverWait(driver, 25, poll_frequency=1,ignored_exceptions=[ElementNotVisibleException, ElementNotSelectableException,NoSuchElementException])
+
+ele = wait.until(lambda x: x.find_element(AppiumBy.ID, "com.code2lead.kwad:id/ScrollView"))
+ele.click()
+
+ele = wait.until(lambda x: x.find_element(AppiumBy.ANDROID_UIAUTOMATOR,'new UiScrollable(new UiSelector()).scrollIntoView(text("BUTTON12"))'))
+ele.click()
+
+time.sleep(4)
+driver.quit()
