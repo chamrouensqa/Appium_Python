616b9f07efb563ee92f9ad1196d3fed16dc2f491
chamrouensann <96223936+chamrouensqa@users.noreply.github.com>
2022-05-20 08:27:17 +0700

    Using android keyboard for keycode to perform action

---
+++ AndroidKeyCode.py
@@ -0,0 +1,35 @@
+import time
+
+from appium import webdriver
+from appium.webdriver.common.appiumby import AppiumBy
+
+desired_caps = {}
+desired_caps['platformName'] = 'Android'
+desired_caps['platformVersion'] = '10'
+desired_caps['deviceName'] = 'Pixel-3'
+desired_caps['automationName'] = 'UiAutomator2'
+desired_caps['appPackage'] = 'com.code2lead.kwad'
+desired_caps['appActivity'] = 'com.code2lead.kwad.MainActivity'
+desired_caps['app'] = ('/Users/chamrouen/Downloads/Android_Demo_App.apk')
+
+driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub',desired_caps)
+ele_id = driver.find_element(AppiumBy.ID,"com.code2lead.kwad:id/EnterValue")
+ele_id.click()
+
+time.sleep(2)
+
+ele_classname = driver.find_element(AppiumBy.CLASS_NAME,"android.widget.EditText")
+ele_classname.send_keys("Skill2lead")
+ele_classname.click()
+
+driver.press_keycode(67)
+driver.press_keycode(67)
+driver.press_keycode(67)
+driver.press_keycode(67)
+driver.press_keycode(67)
+
+ele_sub = driver.find_element(AppiumBy.CLASS_NAME,"android.widget.Button")
+ele_sub.click()
+
+time.sleep(5)
+driver.quit()
